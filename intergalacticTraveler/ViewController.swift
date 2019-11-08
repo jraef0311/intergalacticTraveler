844ecd91987d7544e6620bc2d12e7a45998ac3dd
Jonah Raef <jraef0311@stu.d214.org>
2019-11-08 11:28:02 -0600

    Added second story board and class

--- intergalacticTraveler/ViewController.swift
+++ intergalacticTraveler/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
