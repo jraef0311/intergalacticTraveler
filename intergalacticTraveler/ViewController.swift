ecd9a0cb300b6563c125af247b4835470e2376aa
Jonah Raef <jraef0311@stu.d214.org>
2019-11-12 11:32:04 -0600

    11-12 Lecture, have to make blue and red go to blue and red pictures

--- intergalacticTraveler/ViewController.swift
+++ intergalacticTraveler/ViewController.swift
@@ -9,13 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var colorTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if let data = colorTextField.text,data != "Blue" {
+            let NVC = segue.destination as! SecondViewController
+            NVC.starColor = data
+        }
+        else
+        {
+            let alert = UIAlertController (title: "Error", message: "There was no data", preferredStyle: .alert)
+            let button = UIAlertAction (title: "OK", style: .default, handler: nil)
+            alert.addAction(button)
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
+        
+        
+        
+        
+    }
     
 }
+
 
